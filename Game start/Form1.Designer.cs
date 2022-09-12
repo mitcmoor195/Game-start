ee7799c63a3288ff9810271f41a62f220500f806
mitcmoor195 <mitcmoor195@ed.amdsb.ca>
2022-09-12 11:02:58 -0400

    Add project files.

---
+++ Game start/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Game_start
+{
+    partial class background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(background));
+            this.startButton = new System.Windows.Forms.Button();
+            this.timerSeconds = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Copperplate Gothic Light", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Chartreuse;
+            this.startButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(776, 105);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Play Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timerSeconds
+            // 
+            this.timerSeconds.Font = new System.Drawing.Font("Copperplate Gothic Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerSeconds.ForeColor = System.Drawing.Color.Chartreuse;
+            this.timerSeconds.Location = new System.Drawing.Point(12, 193);
+            this.timerSeconds.Name = "timerSeconds";
+            this.timerSeconds.Size = new System.Drawing.Size(776, 31);
+            this.timerSeconds.TabIndex = 1;
+            this.timerSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timerSeconds);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "background";
+            this.Text = "Game Start";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label timerSeconds;
+    }
+}
+
---
+++ Game start/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Game_start
+{
+    public partial class background : Form
+    {
+        public background()
+        {
+            InitializeComponent();
+        }
+        SoundPlayer player = new SoundPlayer(Properties.Resources.beep);
+        SoundPlayer player2 = new SoundPlayer(Properties.Resources.scifi_warning_beep);
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            Refresh(); 
+            
+            player.Play();
+            timerSeconds.Text = "3..." ;
+            Refresh();
+            Thread.Sleep(1000);
+
+            player.Play();
+            timerSeconds.Text = "2...";
+            Refresh();
+            Thread.Sleep(1000);
+
+            player.Play();
+            timerSeconds.Text = "1...";
+            Refresh();
+            Thread.Sleep(1000);
+
+            player2.Play();
+            timerSeconds.Text = "Begin!";
+            this.BackColor = Color.Purple;
+        }
+    }
+}
